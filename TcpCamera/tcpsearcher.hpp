b54d533df410accdb917dff9cb432bb229b6a0b8
LeoLee <13202336276@qq.com>
2022-04-11 16:18:53 +0800

    20220411 backup

--- TcpCamera/tcpsearcher.hpp
+++ TcpCamera/tcpsearcher.hpp
@@ -12,13 +12,38 @@ class TcpSearcher : public QObject
     Q_OBJECT
 public:
     TcpSearcher(const QString &ip, const int &port) : QObject(nullptr) {
+        found = false;
         for(int i = 0; i < TCP_SEARCH_THREAD_SIZE; i ++) {
             t[i] = new SearchThread(ip, port,
                                     1 + i * 51, (i + 1) * 51,
                                     this);
         }
+        QObject::connect(this, &TcpSearcher::finished,
+                         this, [=](){
+            release();
+        }, Qt::QueuedConnection);
+    }
+    TcpSearcher() {
+        for(int i = 0; i < TCP_SEARCH_THREAD_SIZE; i ++) {
+            t[i] = nullptr;
+        }
+        QObject::connect(this, &TcpSearcher::finished,
+                         this, [=](){
+            release();
+        }, Qt::QueuedConnection);
     }
     ~TcpSearcher() { }
+
+    void search(const QString &ip, const int &port) {
+        found = false;
+        for(int i = 0; i < TCP_SEARCH_THREAD_SIZE; i ++) {
+            if( t[i] == nullptr ) {
+                t[i] = new SearchThread(ip, port,
+                                        1 + i * 51, (i + 1) * 51,
+                                        this);
+            }
+        }
+    }
 
     void release() {
         for(int i = 0; i < TCP_SEARCH_THREAD_SIZE; i ++) {
@@ -55,7 +80,7 @@ private:
             QTcpSocket *s = new QTcpSocket;
             QString search = ip.left(ip.lastIndexOf('.') + 1);
             int index = min;
-            while (!this->isInterruptionRequested()) {
+            while (!this->isInterruptionRequested() && !p->found) {
                 QString devIp = search + QString::number(index);
                 if( devIp == ip ) {
                     index ++;
@@ -65,6 +90,7 @@ private:
                 s->connectToHost(devIp, port);
                 s->waitForConnected(50);
                 if( s->state() == QTcpSocket::ConnectedState ) {
+                    p->found = true;
                     emit p->finished(devIp);
                     break;
                 }
@@ -88,6 +114,7 @@ private:
     };
 
     SearchThread *t[TCP_SEARCH_THREAD_SIZE];
+    bool found;
 
 Q_SIGNALS:
     void finished(const QString &devIp);
